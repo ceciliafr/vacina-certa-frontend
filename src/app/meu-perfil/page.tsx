c0d68b0121ac014613f0e86fd9b9720363598d5d
Cecilia Oliveira <cecilia.oliveira@gympass.com>
2023-11-19 20:40:24 -0300

    feat: add editing fieal

--- src/app/meu-perfil/page.tsx
+++ src/app/meu-perfil/page.tsx
@@ -5,10 +5,7 @@ import { DesktopMenu } from "@/components/desktop/Menu";
 import { DesktopNavbar } from "@/components/desktop/Navbar";
 import { Layout } from "@/components/Layout";
 import Box from "@mui/material/Box";
-import EditRoundedIcon from "@mui/icons-material/EditRounded";
-import IconButton from "@mui/material/IconButton";
-import Image from "next/image";
-import { yellow } from "@mui/material/colors";
+import { EditableLabel } from "@/components/Input";
 
 export default function MyProfile() {
   return (
@@ -18,8 +15,8 @@ export default function MyProfile() {
         <div className={styles.right_content}>
           <Box>
             <Title>
-              Bem-vindo(a) à sua área de dados pessoais, você pode alterar seus
-              dados a qualquer momento:
+              Olá, Cissa! Essa é sua área de dados pessoais, você pode alterar
+              seus dados a qualquer momento:
             </Title>
           </Box>
           <Box
@@ -29,105 +26,35 @@ export default function MyProfile() {
             gap="5rem"
             marginTop="6rem"
           >
-            {/* <div style={{ maxWidth: "100rem" }}>
-              <div
-                style={{
-                  display: "flex",
-                  justifyContent: "center",
-                  position: "relative",
-                  width: "100%",
-                }}
-              >
-                <Image
-                  src="/edit_user4.png"
-                  alt="vacina_certa_logo"
-                  width={600}
-                  height={1000}
-                  style={{
-                    width: "50%",
-                    height: "auto",
-                  }}
-                  sizes="100vw"
-                  // fill
-                />
-              </div>
-            </div> */}
-            {/* <div className={styles.vertical_line}></div> */}
-            <Box display="flex" flexDirection="column" height="100%">
+            <Box
+              display="flex"
+              flexDirection="column"
+              height="100%"
+              width="100%"
+            >
               <div className={styles.user_info_container}>
-                <Box
-                  display="flex"
-                  alignItems="center"
-                  gap={2}
-                  marginBottom={4}
-                >
-                  <h2 className={styles.user_name}>
-                    Cecília Fernandes de Oliveira
-                  </h2>
-                  <IconButton
-                    aria-label="edit"
-                    size="small"
-                    className={styles.edit_button}
-                  >
-                    <EditRoundedIcon fontSize="inherit" color="action" />
-                  </IconButton>
-                </Box>
+                <EditableLabel lableName="CPF" labelValue="114.255.916-55" />
+                <EditableLabel
+                  lableName="Data de Nascimento"
+                  labelValue="15/06/2000"
+                />
+                <EditableLabel
+                  lableName="Seu Nome"
+                  labelValue="Cecília Fernandes de Oliveira"
+                  isEditable
+                />
 
+                <EditableLabel
+                  lableName="Telefole"
+                  labelValue="+55 (31) 99285-0842"
+                  isEditable
+                />
                 <div className={styles.personal_data}>
-                  <Box>
-                    <h4 className={styles.label_title}>CPF</h4>
-                    <h4 className={styles.label_value}>114.255.916-55</h4>
-                  </Box>
-                  <Box>
-                    <h4 className={styles.label_title}>Data de Nascimento</h4>
-                    <h4 className={styles.label_value}>15/06/2000</h4>
-                  </Box>
-
-                  <div>
-                    <h4 className={styles.label_title}>Telefole</h4>
-                    <Box display="flex" alignItems="center" gap={2}>
-                      <h4 className={styles.label_value}>
-                        +55 (31) 99285-0842
-                      </h4>
-
-                      <IconButton
-                        aria-label="edit"
-                        size="small"
-                        className={styles.edit_button}
-                      >
-                        <EditRoundedIcon fontSize="inherit" />
-                      </IconButton>
-                    </Box>
-                  </div>
-                  <div>
-                    <h4 className={styles.label_title}>
-                      Como gostaria que chamassemos você
-                    </h4>
-                    <Box display="flex" alignItems="center" gap={2}>
-                      <h4 className={styles.label_value}>Cissa</h4>
-                      <IconButton
-                        aria-label="edit"
-                        size="small"
-                        className={styles.edit_button}
-                      >
-                        <EditRoundedIcon fontSize="inherit" />
-                      </IconButton>
-                    </Box>
-                  </div>
-
-                  {/* <div>
-                    <h4 className={styles.label_title}>Senha</h4>
-                    <Box display="flex" alignItems="baseline" gap={2}>
-                      <h4 className={styles.label_value}>*********</h4>
-                      <IconButton
-                        aria-label="edit"
-                        size="small"
-                        className={styles.edit_button}
-                      >
-                        <EditRoundedIcon fontSize="inherit" />
-                      </IconButton>
-                    </Box>
-                  </div> */}
+                  <EditableLabel
+                    lableName="Como gostaria que chamassemos você"
+                    labelValue="Cissa"
+                    isEditable
+                  />
                 </div>
               </div>
             </Box>
